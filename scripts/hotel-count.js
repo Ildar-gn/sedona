815c214b31e5173f832c8a9cfef137fca7be7647
Ildar <iledar@icloud.com>
2025-02-16 21:28:50 +0500

    Обновляет стили и структуру HTML, добавляет функциональность для кнопок, изменяет генерацию случайного числа гостиниц и обновляет SVG-изображения

--- scripts/hotel-count.js
+++ scripts/hotel-count.js
@@ -1,5 +1,8 @@
 // Генерация случайного числа от 1 до 100
 var randomCount = Math.floor(Math.random() * 100) + 1;
 
+// Смещаем диапазон, чтобы начать с 4, но не превышать 100
+randomCount = Math.min(randomCount + 3, 100);
+
 // Обновление значения в элементе <output>
 document.getElementById("hotel-count").value = randomCount;
